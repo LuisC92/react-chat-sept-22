1489310a3fea0f39f7b6569f959043ba116ce8ef
LuisC92 <luis.clara@wildcodeschool.com>
2023-01-05 09:16:01 +0000

    setup repo for workshop about socket.io

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  //* states for messagesDB, newMessages, nickname, socket
+  
+  return (
+    <div className="App">
+      <h2>Messages</h2>
+     {/* form to send nickname and messages   */}
+    </div>
+  );
+}
+
+export default App;
